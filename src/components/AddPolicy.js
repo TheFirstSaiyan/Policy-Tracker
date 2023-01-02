d1cbfbd3836343496f02dd614da5cea1ef511231
TheFirstSaiyan <19hari98@gmail.com>
2023-01-02 15:14:53 +0530

    frontend complete

--- src/components/AddPolicy.js
+++ src/components/AddPolicy.js
@@ -1,63 +1,104 @@
 
 import NomineeForm from './NomineeForm'
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 function AddPolicy() {
 
-    const [showNomineeForm,setShowNomineeForm] = useState(false);
+    const [showNomineeForm, setShowNomineeForm] = useState(false);
+
+    const [policyName, setPolicyName] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [lastDate, setLastDate] = useState('');
+    const [tenure, setTenure] = useState('');
+    const [premiumType, setPremiumType] = useState('Monthly');
+    const [premiumAmount, setPremiumAmount] = useState(0);
+    const [policyDetails, setPolicyDetails] = useState('');
+    const [nominees, setNominees] = useState([]);
+
+    function checkLength() {
+        console.log( premiumType);
+        return policyName.length > 0 && policyDetails.length > 0 && premiumType.length > 0;
+    }
+
+    function checkInvalidChars() {
+        let re = new RegExp("^[0-9]+$");
+        return re.test(tenure) && re.test(premiumAmount);
+    }
+
+    function checkDate() {
+        let re = new RegExp("^(0[123456789]|10|11|12)([/])([1-2][0-9][0-9][0-9])$");
+        return re.test(startDate) && re.test(lastDate);
+    }
+    function handleSubmit() {
+        if (checkLength() && checkDate() && checkInvalidChars()) 
+        {
+            setPolicyName('');
+            setStartDate('');
+            setLastDate('');
+            setTenure('');
+            setPremiumType('Monthly');
+            setPremiumAmount(0);
+            setPolicyDetails('');
+            setNominees([]);
+        }
+        else
+        {
+            alert("Please give valid input");
+        }
+    }
 
 
     return (
         <form>
             <div>
                 <label className='mt-5 form-label'>Policy Name</label>
-                <input type='text' className='form-control' required></input>
+                <input type='text' className='form-control' required value={policyName} onChange={(e) => (setPolicyName(e.target.value))}></input>
             </div>
-            
+
 
             <div className="row">
 
                 <div className="col-md-4">
                     <label className='mt-5 form-label'>Start date(mm/yyyy)</label>
-                    <input type='text' className='form-control' required></input>
+                    <input type='text' className='form-control' required value={startDate} onChange={(e) => (setStartDate(e.target.value))}></input>
                 </div>
                 <div className="col-md-4">
                     <label className='mt-5 form-label'>Tenure(in years)</label>
-                    <input type='number' className='form-control' required min = {1}></input>
+                    <input type='number' className='form-control' required min={1} value={tenure} onChange={(e) => (setTenure(e.target.value))}></input>
                 </div>
                 <div className="col-md-4">
                     <label className='mt-5 form-label'>Last date(mm/yyyy)</label>
-                    <input type='text' className='form-control' required></input>
+                    <input type='text' className='form-control' required value={lastDate} onChange={(e) => (setLastDate(e.target.value))}></input>
                 </div>
             </div>
             <div className="row">
                 <div className="col-md-4">
                     <label className='mt-5 form-label'>Premium Type</label>
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Monthly</option>
-                        <option value="2">Yearly</option>
-                        <option value="3">Once</option>
+                    <select class="form-select" aria-label="Default select example" onChange={(e) => (setPremiumType(e.target.value))}>
+                        <option selected value='Monthly'>Monthly</option>
+                        <option value="Yearly">Yearly</option>
+                        <option value="Once">Once</option>
                     </select>
                 </div>
 
                 <div className="col-md-4">
                     <label className='mt-5 form-label'>premium amount (in Rupees)</label>
-                    <input type='text' className='form-control' required></input>
+                    <input type='text' className='form-control' required value={premiumAmount} onChange={(e) => (setPremiumAmount(e.target.value))}></input>
                 </div>
             </div>
             <div>
                 <label className='mt-5 form-label'>Policy Description and Return details</label>
-                <textarea type='text-area' rows={4} className='form-control' required></textarea>
+                <textarea type='text-area' rows={4} className='form-control' required value={policyDetails} onChange={(e) => (setPolicyDetails(e.target.value))}></textarea>
             </div>
 
-            <NomineeForm setShowNomineeForm = {setShowNomineeForm} showNomineeForm = {showNomineeForm}/>
+            <NomineeForm setShowNomineeForm={setShowNomineeForm} showNomineeForm={showNomineeForm} nominees={nominees} setNominees={setNominees} />
             <div className='text-end'>
-                {!showNomineeForm && <button type='button' className='mt-5 mb-2 btn btn-primary' onClick={()=>{setShowNomineeForm(!showNomineeForm)}}> + Nominee(s)</button>}
+                {!showNomineeForm && <button type='button' className='mt-5 mb-2 btn btn-primary' onClick={() => { setShowNomineeForm(!showNomineeForm) }}> + Nominee(s)</button>}
             </div>
 
 
             <div className='text-center'>
-                <button type='button' className='mt-5 btn btn-success' >Submit</button>
+                <button type='button' className='mt-5 btn btn-success' onClick={handleSubmit} >Submit</button>
             </div>
 
         </form>
--- src/components/NomineeForm.js
+++ src/components/NomineeForm.js
@@ -1,32 +1,33 @@
 
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import NomineeDetails from "./NomineeDetails";
 
 function NomineeForm(props) {
-    const [nominees, setNominees] = useState([]);
+    
     const [nomineeName, setNomineeName] = useState('');
 
     function handleClick() {
         
-        setNominees(nominees => [...nominees, {id :nominees.length > 0 ? nominees[nominees.length-1].id + 1 : 1, name :nomineeName}]);
+        props.setNominees(nominees => [...props.nominees, {id :nominees.length > 0 ? props.nominees[props.nominees.length-1].id + 1 : 1, name :nomineeName}],);
         setNomineeName('');
         props.setShowNomineeForm(false);
 
     }
 
+
     return (
         <div className="container">
             <div>
-                <NomineeDetails nominees = {nominees} setNominees = {setNominees}/>
+                <NomineeDetails nominees = {props.nominees} setNominees = {props.setNominees}/>
                 
             </div>
             {props.showNomineeForm && <form>
                 <div className="row mt-5  d-flex  justify-content-center align-items-center shadow p-3 mb-5 bg-light rounded">
-                    <div className="col-md-5  d-flex  align-items-center" >
-                        <label className='form-label m-2'>Nominee(s)</label>
+                    <div className="col-md-5   d-flex  align-items-center" >
+                        <label className='form-label m-2'>Nominee</label>
                         <input type='text' className='form-control' required value={nomineeName} onChange={(e) => setNomineeName(e.target.value)}></input>
                     </div>
-                    <div className="col-md-2">
+                    <div className="col-md-2 ">
                         <button type='button' className=' btn btn-success btn-sm' onClick={handleClick}>+</button>
                     </div>
                 </div>
